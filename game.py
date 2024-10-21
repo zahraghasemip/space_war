d016c49d6f281b4789f73e3fe42976226a4966dc
Zahra Ghasemi <zahraghasemi2487@gmail.com>
2024-10-21 15:43:57 +0330

    player added

--- game.py
+++ game.py
@@ -2,10 +2,20 @@ import pygame
 pygame.init()
 
 #main game
-screen = pygame.display.set_mode((800,400))
+screen = pygame.display.set_mode((500,600))
 
 #title & icon
 pygame.display.set_caption("Space War")
+
+icon = pygame.image.load('./files/icon.png')
+pygame.display.set_icon(icon)
+#player
+playerImg = pygame.image.load('./files/player.png')
+playerX = 225
+playerY = 450
+
+def player():
+    screen.blit(playerImg,(playerX,playerY))
 
 #frames
 running = True
@@ -13,3 +23,7 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+    
+    screen.fill((102,178,255))
+    player()
+    pygame.display.update()
