6f1da9bc05dd1c13dddc18a57862d32ae997399a
Zahra Ghasemi <zahraghasemi2487@gmail.com>
2024-10-21 16:08:26 +0330

    keyboard movement for player added

--- game.py
+++ game.py
@@ -13,9 +13,9 @@ pygame.display.set_icon(icon)
 playerImg = pygame.image.load('./files/player.png')
 playerX = 225
 playerY = 450
-
-def player():
-    screen.blit(playerImg,(playerX,playerY))
+playerX_change = 0
+def player(x,y):
+    screen.blit(playerImg,(x,y))
 
 #frames
 running = True
@@ -23,7 +23,22 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT:
+                playerX_change = 0.2
+            if event.key == pygame.K_LEFT:
+                playerX_change = -0.2
+
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_RIGHT or event.type == pygame.K_LEFT:
+                playerX_change = 0
     
     screen.fill((102,178,255))
-    player()
+    playerX += playerX_change
+    if playerX <= 0:
+        playerX = 0
+    elif playerX >= 436:
+        playerX = 436
+
+    player(playerX,playerY)
     pygame.display.update()
